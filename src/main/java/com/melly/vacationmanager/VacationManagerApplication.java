33bba451f4ce13685149e185f10889139a34631f
haily <49448330+melly8954@users.noreply.github.com>
2025-07-09 17:56:33 +0900

    init 250709

---
+++ src/main/java/com/melly/vacationmanager/VacationManagerApplication.java
@@ -0,0 +1,13 @@
+package com.melly.vacationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacationManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/melly/vacationmanager/VacationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melly.vacationmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
