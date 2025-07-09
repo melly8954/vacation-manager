8ba65165b8a1cb71bdf0d54358f0192620565ae6
haily <49448330+melly8954@users.noreply.github.com>
2025-07-10 00:25:32 +0900

    refactor: csrf 설정 람다식을 메서드 참조로 변환 (250710)

--- src/main/java/com/melly/vacationmanager/global/config/SecurityConfig.java
+++ src/main/java/com/melly/vacationmanager/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -15,7 +16,7 @@ public class SecurityConfig {
         // 메서드 체이닝은 (.) 기준으로 줄 바꿈 사용
         // 람다 식 파라미터가 하나일 때 괄호를 생략 가능
         http
-                .csrf(csrf ->csrf.disable());
+                .csrf(AbstractHttpConfigurer::disable);     // 람다식을 메서드 레퍼런스로 치환
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/").permitAll()
