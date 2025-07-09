0bb53b89d51adacbcdcbc85f9dbe5d0478ee24d5
haily <49448330+melly8954@users.noreply.github.com>
2025-07-10 00:07:25 +0900

    feat: 공통 컨트롤러 AllController 추가 (250710)
    HTML 뷰 반환용 컨트롤러를 한 곳에 모아 관리할 AllController 구현

---
+++ src/main/java/com/melly/vacationmanager/global/common/controller/AllController.java
@@ -0,0 +1,12 @@
+package com.melly.vacationmanager.global.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AllController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
