423d3e4b1a03aec1cb05ab5d0288b8456c7f9fa0
haily <49448330+melly8954@users.noreply.github.com>
2025-07-10 23:01:53 +0900

    feat: role, status Enum 클래스 description 추가 (250710)

--- src/main/java/com/melly/vacationmanager/global/common/enums/UserRole.java
+++ src/main/java/com/melly/vacationmanager/global/common/enums/UserRole.java
@@ -1,6 +1,15 @@
 package com.melly.vacationmanager.global.common.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum UserRole {
-    USER,
-    ADMIN
+    USER("사용자"),
+    ADMIN("관리자");
+
+    private final String description;
+
+    UserRole(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/com/melly/vacationmanager/global/common/enums/UserStatus.java
+++ src/main/java/com/melly/vacationmanager/global/common/enums/UserStatus.java
@@ -1,7 +1,16 @@
 package com.melly.vacationmanager.global.common.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum UserStatus {
-    PENDING,
-    ACTIVE,
-    REJECTION
+    PENDING("승인 대기"),
+    ACTIVE("승인 완료"),
+    REJECTION("승인 반려");
+
+    private final String description;
+
+    UserStatus(String description) {
+        this.description = description;
+    }
 }
